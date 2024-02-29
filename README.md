c94792b890de767b7ad834ac87e955385a653d95
Isaac Munoz <isaac.munoz@iepcjalisco.mx>
2024-02-29 14:40:15 -0600

    Se realizan configuraciones para la conexion de postgresql y express.js

--- README.md
+++ README.md
@@ -2,8 +2,34 @@
 MediDash is software designed to efficiently manage medication distribution in hospital environments. This system facilitates coordination between medical staff, improves the availability of medications and reduces patient waiting times. With features ranging from inventory tracking to medication delivery optimization
 
 ## Config Node.js
-puerto 3000
+puerto 3001
 
 ## Config Postgresql
 password:medidash
 puerto 5432
+
+## Valida su funcionamiento
+
+
+Instala el gestor de dependencias de Node.js
+```bash
+npm install
+```
+
+Instala dependencia para postgresql (esto en caso de que clone el repo nuevamente y no lo tengas instalado)
+```bash
+npm install pg
+```
+
+Ejecuta la aplicacion 
+
+``` bash
+    node server.js
+```
+
+Despues inicia el contenedor
+
+``` bash
+    docker-compose up
+
+```
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,40 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const { Pool } = require('pg');
 
-app.get('/', (req, res) => {
-    res.send('¡Hola desde Express.js!');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configuración de la conexión a la base de datos PostgreSQL
+const pool = new Pool({
+    user: 'postgres',
+    host: 'postgres',
+    database: 'medidash_db',
+    password: 'medidash',
+    port: 5432,
 });
 
+// Manejo de errores de conexión
+pool.on('error', (err, client) => {
+    console.error('Error inesperado en la conexión', err);
+    process.exit(-1);
+});
+
+// Middleware para manejar JSON en solicitudes
+app.use(express.json());
+
+// Ruta para verificar la conexión con la base de datos
+app.get('/api/healthcheck', async (req, res) => {
+    try {
+        await pool.query('SELECT 1');
+        res.status(200).json({ message: 'Conexión exitosa con la base de datos' });
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error);
+        res.status(500).json({ error: 'Error al conectar con la base de datos' });
+    }
+});
+
+// Puerto de escucha
 app.listen(PORT, () => {
     console.log(`Servidor Express corriendo en el puerto ${PORT}`);
 });
+
