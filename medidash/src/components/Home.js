a8e504df6307e8befdbd4816c00383e10449e91f
Isaac Munoz <isaac.munoz@iepcjalisco.mx>
2024-03-05 17:07:47 -0600

    se crea rama login y se comienza a trabajar en ella

---
+++ medidash/src/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = (props) => {
+    const { loggedIn, email } = props
+    const navigate = useNavigate()
+
+    const onButtonClick = () => {
+        if (loggedIn) {
+        localStorage.removeItem('user')
+        props.setLoggedIn(false)
+        } else {
+        navigate('/login')
+        }
+    }
+
+    return (
+        <div className="mainContainer">
+            <div className={'titleContainer'}>
+                <div>Medidash</div>
+            </div>
+            <br/>
+            <div>Gestión de medicamentos, salud optimizada.</div>
+            <div className={'buttonContainer'}>
+                <input
+                className={'inputButton'}
+                type="button"
+                onClick={onButtonClick}
+                value={loggedIn ? 'Log out' : 'Log in'}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Home
